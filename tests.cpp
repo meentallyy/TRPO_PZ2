45cf882b02d65f3ccf048536de7b0c34c6a98d09
meentallyy <a.galk.2019@yandex.ru>
2025-10-30 05:30:01 +0300

    Добавление тестов

---
+++ tests.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+    cout << "Запуск тестов" << endl;
+
+    assert(Add(2, 3) == 5);
+    assert(Add(-1, 1) == 0);
+
+    assert(Subtract(10, 4) == 6);
+    assert(Subtract(0, 5) == -5);
+
+    assert(Multiply(3, 5) == 15);
+    assert(Multiply(-2, 4) == -8);
+
+    assert(Divide(10, 2) == 5);
+    assert(Divide(5, 2) == 2.5);
+
+    cout << "Все тесты пройдены." << endl;
+    return 0;
+}
